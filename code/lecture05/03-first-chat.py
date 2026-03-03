56e1577475e4ed7a9169dd03d1e3d37989b2ccc1
Daniel Chen <chendaniely@users.noreply.github.com>
2026-03-03 15:04:09 -0800

    move examples into the main lecture05 folder

--- code/lecture05/03-first-chat.py
+++ code/lecture05/03-first-chat.py
@@ -4,7 +4,11 @@ from chatlas import ChatGithub
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(Path(__file__).parent / ".env")
+try:
+    load_dotenv(Path(__file__).parent / ".env")
+except NameError:
+    load_dotenv()
+    load_dotenv(Path("code/lecture05/demo01/.env"))
 
 chat = ChatGithub(
     model="gpt-4.1",
